7c5f8b01b61a2dd6e382ee7c81db231583206951
narendra <sonwatkar777@gmail.com>
2023-04-24 02:27:26 +0530

    v1.01

--- src/componant/Main/Main.jsx
+++ src/componant/Main/Main.jsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 import Navbar from "../parts/Navbar";
 import Body from "../parts/Body";
-import Footer from "./parts/Footer";
+import Footer from "../parts/Footer";
 
 const Main = () => {
   return (
